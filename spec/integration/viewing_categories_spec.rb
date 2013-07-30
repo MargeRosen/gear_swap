3081506e159de1a6f25eb03136c204561939ff89
Kathryn <keb97@cornell.edu>
2013-07-30 11:11:06 -0700

    authorizing for signed in users, feature in progress.

--- spec/integration/viewing_categories_spec.rb
+++ spec/integration/viewing_categories_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
   feature "Viewing categories" do
+    let!(:user) {Factory(:confirmed_user)}
+    let!(:category) {Factory(:category)}
+
+    before do
+      sign_in_as!(user)
+      define_permission!(user, :view, category)
+    end
+
+
     scenario "Viewing all categories" do
-      category = Factory.create(:category, :name => "Keyboards")
       visit '/'
       click_link category.name
       page.current_url.should == category_url(category)
