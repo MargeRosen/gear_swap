233c3d3ebd27dc2f44c2dea5ce308d5f34622f83
Kathryn <keb97@cornell.edu>
2013-07-30 10:01:56 -0700

    fixed up application view after removing twitter bootstrap-generated template, added flash msg if user navs to nonexistant category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_filter :find_category, :only => [:show, :edit, :update, :destroy]
+
   def index
     @categories = Category.all
   end
@@ -19,15 +21,12 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update_attributes(params[:category])
       flash[:notice] = "Category has been updated."
       redirect_to @category
@@ -38,10 +37,16 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     @category.destroy
     flash[:notice] = "Category has been deleted."
     redirect_to categories_path
   end
 
+  private
+    def find_category
+      @category = Category.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The category you were looking for could not be found."
+      redirect_to categories_path
+    end
 end
