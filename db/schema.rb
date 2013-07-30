962897b2bf318cbe62307699d042d9d9734e97b6
Kathryn <keb97@cornell.edu>
2013-07-30 11:25:06 -0700

    working on Permission model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130730162953) do
+ActiveRecord::Schema.define(:version => 20130730181540) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(:version => 20130730162953) do
 
   add_index "listings", ["category_id"], :name => "index_listings_on_category_id"
   add_index "listings", ["user_id"], :name => "index_listings_on_user_id"
+
+  create_table "permissions", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "thing_id"
+    t.string   "thing_type"
+    t.string   "action"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
