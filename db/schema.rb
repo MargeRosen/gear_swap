9dc767326ed91e4cfb80a2be125c04afdac034df
Kathryn <keb97@cornell.edu>
2013-08-26 16:59:51 -0700

    can upload multiple images using nested attributes and carrierwave gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130826180358) do
+ActiveRecord::Schema.define(:version => 20130826215038) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "images", :force => true do |t|
+    t.string   "file"
+    t.integer  "attachable_id"
+    t.string   "attachable_type"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "images", ["attachable_id"], :name => "index_images_on_attachable_id"
 
   create_table "listings", :force => true do |t|
     t.string   "title"
