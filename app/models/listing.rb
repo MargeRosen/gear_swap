e6d7abae59a21fed5978189e0b231b66107baeb3
Kathryn <keb97@cornell.edu>
2013-08-26 11:43:03 -0700

    in development you can add one picture per listing using carrierwave gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,8 @@
 class Listing < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
-  attr_accessible :contact, :description, :location, :pics, :pics2, :pics3, :price, :title
+  attr_accessible :contact, :description, :location, :pics, :pics2, :pics3, :price, :title, :image
   validates :title, :presence => true
   validates :description, :presence => true
+  mount_uploader :image, ImageUploader
 end
