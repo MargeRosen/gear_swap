eae0dce218574c88a8b33e46f92b1725a32b5c51
Kathryn <keb97@cornell.edu>
2013-08-29 12:46:54 -0700

    trying to successfully send emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "keb97@cornell.edu"
+  config.mailer_sender = "feedback@axswap.com"
 
   # Configure the class responsible to send e-mails.
   config.mailer = "Devise::Mailer"
